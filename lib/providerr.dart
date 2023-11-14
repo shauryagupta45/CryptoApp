33a37a28ac14708fe72ad0b361317cea1d1aa06c
shauryagupta45 <shauryagupta4501@gmail.com>
2023-11-14 13:18:50 +0530

    update

--- lib/providerr.dart
+++ lib/providerr.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 
 class OptionProvider with ChangeNotifier {
-  static String _selectedOption = 'Filter';
+  static String _selectedOption = 'Filter ';
   String get selectedOption => _selectedOption;
 
   void setOption(String selectedOption) {
